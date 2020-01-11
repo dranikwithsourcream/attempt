fa506ec7e844ea4888ba896b39765c199f11702f
Andrew <59490330+dranikwithsourcream@users.noreply.github.com>
2020-01-11 14:49:13 +0200

    Update program.py

---
+++ program.py
@@ -0,0 +1,19 @@
+import json
+
+filename = money_data.txt
+myfile = open(filename, mode='w', encoding='Latin-1')
+
+man1 = {
+    'Skin': "Black"
+    'Name': "John"
+}
+
+man2 = {
+    'Skin': "White"
+    'Name': "Nick"
+}
+
+people =[]
+people.append(man1)
+people.append(man2)
+
