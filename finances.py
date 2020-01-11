3f00fcb07b1a19ac5e1c6f86f8a9380430852208
Andrew <59490330+dranikwithsourcream@users.noreply.github.com>
2020-01-11 21:29:15 +0200

    Update finances.py

--- finances.py
+++ finances.py
@@ -1,11 +1,21 @@
 from colorama import Fore, Back, Style
+import os
 
 #Back.GREEN + Fore.BLACK +
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def operations():
-    print('1')
+    print('Выберите операцию:')
 
 def statistics():
-    print('2')
+    print('Выберите статистику:')
+    print('1 - За 1 день.')
+    print('2 - За 3 дня.')
+    print('3 - За 7 дней.')
+    print('4 - За 14 дней.')
+    print('5 - За 31 день.')
 
 def intro():
     print('1 - Операции со средствами.')
