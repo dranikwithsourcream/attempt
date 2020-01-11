3bd380ef2a8fa9bcb9079c07591ced2df1a1155f
Andrew <59490330+dranikwithsourcream@users.noreply.github.com>
2020-01-11 22:10:44 +0200

    Update data.py

--- data.py
+++ data.py
@@ -2,11 +2,11 @@ import sqlite3
 con = sqlite3.connect('database.db')
 cur = con.cursor()
 
-#cur.execute("""CREATE TABLE employees (
-#            first text,
-#            last text,
-#            pay integer
-#            )""")
+cur.execute("""CREATE TABLE employees (
+            first text,
+            last text,
+            pay integer
+            )""")
 
 #cur.execute("INSERT INTO employees VALUES ('Andrew', 'Derevianko', 99999)")
 
